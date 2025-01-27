f226745cacbb564a96051b6c5db75d437b248894
hs55552k <hs55552k@gmail.com>
2025-01-27 23:47:13 +0500

    clone completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import Main from "./components/main/Main";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
 
-export default App
+export default App;
